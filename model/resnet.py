27c4d6bc23de3bdb0ee8a8558dc3da72798646c8
Pawel Skiers <pawelsk269@gmail.com>
2023-06-03 02:42:10 +0200

    Add pooling, first conv

--- model/resnet.py
+++ model/resnet.py
@@ -29,7 +29,12 @@ class ResBlock(nn.Module):
         self.downsample: None | nn.Module = None
         if stride != 0:
             self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1),
+                nn.Conv2d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=1,
+                    stride=stride
+                ),
                 nn.BatchNorm2d(out_channels)
             )
         self.relu = nn.ReLU()
@@ -45,14 +50,17 @@ class ResBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block_list: list[dict]) -> None:
+    def __init__(self, first_conv: dict, block_list: list[dict], pool_size: int) -> None:
         super().__init__()
+        self.first_conv = nn.Conv2d(**first_conv)
         self.res_blocks = nn.ModuleList([
             ResBlock(**block_conf) for block_conf in block_list
         ])
+        self.pooling = nn.AvgPool2d(kernel_size=pool_size, stride=pool_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out = x
+        out = self.first_conv(x)
         for block in self.res_blocks:
             out = block(out)
+        out = self.pooling(out)
         return torch.flatten(out, start_dim=1)
