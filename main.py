c726f2bacbf7d1ea142084722070f7b3e6af85c8
Pawel Skiers <pawelsk269@gmail.com>
2023-06-12 19:02:57 +0200

    Add recall and precision at k metrics

--- main.py
+++ main.py
@@ -1,101 +1,101 @@
 from os import path
 import torch
-# from omegaconf import OmegaConf
-# from model.model import KLLossMetricLearning
-# import pytorch_lightning as pl
-# from pytorch_lightning.loggers.wandb import WandbLogger
-# import torchvision.transforms as transforms
-# from datetime import datetime
-# from data_utils import cub2011
-# from data_utils import stanford_cars
+from omegaconf import OmegaConf
+from model.model import KLLossMetricLearning
+import pytorch_lightning as pl
+from pytorch_lightning.loggers.wandb import WandbLogger
+import torchvision.transforms as transforms
+from datetime import datetime
+from data_utils import cub2011
+from data_utils import stanford_cars
 from pytorch_metric_learning import miners, losses
 from model.model import KLDistance, KL_d, KLoss, KLLossMetricLearning
 
 
 if __name__ == "__main__":
-    # IMAGE_SIZE = 224
-    # BATCH_SIZE = 64
+    IMAGE_SIZE = 224
+    BATCH_SIZE = 64
 
-    # train_transform = transforms.Compose([
-    #     # transforms.RandomHorizontalFlip(p=0.5),
-    #     # transforms.RandomRotation(30),
-    #     # transforms.RandomResizedCrop(224, scale=(0.7, 1), ratio=(3/4, 4/3)),
-    #     transforms.Resize(IMAGE_SIZE),
-    #     transforms.CenterCrop(IMAGE_SIZE),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-    #                          std=[0.229, 0.224, 0.225])
-    # ])
-
-    # transform = transforms.Compose([
-    #     transforms.Resize(IMAGE_SIZE),
-    #     transforms.CenterCrop(IMAGE_SIZE),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-    #                          std=[0.229, 0.224, 0.225])
-    # ])
-
-    # # cars_trainset = stanford_cars.download_cars('./data', train=True, download=True, transforms=train_transform)
-    # # cars_testset = stanford_cars.download_cars('./data', train=True, download=False, transforms=transform)
-    # cub_trainset = cub2011.download_cub('./data', train=True, download=True, transforms=train_transform)
-    # cub_testset = cub2011.download_cub('./data', train=False, download=False, transforms=transform)
-
-    # # cars_trainloader = torch.utils.data.DataLoader(cars_trainset, batch_size=BATCH_SIZE,
-    # #                                           shuffle=True, drop_last=True)
-    # # cars_testloader = torch.utils.data.DataLoader(cars_testset, batch_size=BATCH_SIZE,
-    # #                                           shuffle=False, drop_last=True)
-    # cub_trainloader = torch.utils.data.DataLoader(cub_trainset, batch_size=BATCH_SIZE,
-    #                                           shuffle=True, drop_last=True)
-    # cub_testloader = torch.utils.data.DataLoader(cub_testset, batch_size=BATCH_SIZE,
-    #                                           shuffle=False, drop_last=True)
-
-    # conf_path = path.join("configs", "model_cub.yaml")
-    # conf = OmegaConf.load(conf_path)
-    # model = KLLossMetricLearning(**conf.get("model"))
-
-    # pl.seed_everything(42)
-    # now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-    # nowname = model.__class__.__name__ + "_" + now
-    # logdir = path.join("logs", nowname)
-    # ckptdir = path.join(logdir, "checkpoints")
-    # default_modelckpt_cfg = {
-    #     "dirpath": ckptdir,
-    #     "filename": "{epoch:06}",
-    #     "verbose": True,
-    #     "save_last": True,
-    #     "monitor": "val/loss",
-    #     "save_top_k": 1,
-    #     # "mode": "max",
-    # }
-    # callbacks = [pl.callbacks.ModelCheckpoint(**default_modelckpt_cfg)]
-    # logger = WandbLogger(
-    #     project="ZZSN",
-    #     name=nowname,
-    #     log_model=True,
-    #     id=nowname,
-    #     # group=str(group),
-    #     reinit=True,
-    #     allow_val_change=True
-    # )
-    # trainer = pl.Trainer(logger=logger, callbacks=callbacks, max_epochs=100)
-    # trainer.fit(
-    #     model, train_dataloaders=cub_trainloader, val_dataloaders=cub_testloader
-    # )
-    means = torch.Tensor([
-        [3, 0, 3],
-        [2, 0, 2],
-        [7, 3, 2],
-        [3, 2, 1]
-        ])
-    stds = torch.Tensor([
-        [0.2, 0.3, 0.1],
-        [0.3, 0.1, 2],
-        [0.1, 0.1, 0.1],
-        [0.2, 0.3, 0.5]
+    train_transform = transforms.Compose([
+        # transforms.RandomHorizontalFlip(p=0.5),
+        # transforms.RandomRotation(30),
+        # transforms.RandomResizedCrop(224, scale=(0.7, 1), ratio=(3/4, 4/3)),
+        transforms.Resize(IMAGE_SIZE),
+        transforms.CenterCrop(IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
     ])
-    labels = torch.Tensor(
-        [2, 2, 3, 3]
+
+    transform = transforms.Compose([
+        transforms.Resize(IMAGE_SIZE),
+        transforms.CenterCrop(IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    # cars_trainset = stanford_cars.download_cars('./data', train=True, download=True, transforms=train_transform)
+    # cars_testset = stanford_cars.download_cars('./data', train=True, download=False, transforms=transform)
+    cub_trainset = cub2011.download_cub('./data', train=True, download=True, transforms=train_transform)
+    cub_testset = cub2011.download_cub('./data', train=False, download=False, transforms=transform)
+
+    # cars_trainloader = torch.utils.data.DataLoader(cars_trainset, batch_size=BATCH_SIZE,
+    #                                           shuffle=True, drop_last=True)
+    # cars_testloader = torch.utils.data.DataLoader(cars_testset, batch_size=BATCH_SIZE,
+    #                                           shuffle=False, drop_last=True)
+    cub_trainloader = torch.utils.data.DataLoader(cub_trainset, batch_size=BATCH_SIZE,
+                                              shuffle=True, drop_last=True)
+    cub_testloader = torch.utils.data.DataLoader(cub_testset, batch_size=BATCH_SIZE,
+                                              shuffle=False, drop_last=True)
+
+    conf_path = path.join("configs", "model_cub.yaml")
+    conf = OmegaConf.load(conf_path)
+    model = KLLossMetricLearning(**conf.get("model"))
+
+    pl.seed_everything(42)
+    now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    nowname = model.__class__.__name__ + "_" + now
+    logdir = path.join("logs", nowname)
+    ckptdir = path.join(logdir, "checkpoints")
+    default_modelckpt_cfg = {
+        "dirpath": ckptdir,
+        "filename": "{epoch:06}",
+        "verbose": True,
+        "save_last": True,
+        "monitor": "val/loss",
+        "save_top_k": 1,
+        # "mode": "max",
+    }
+    callbacks = [pl.callbacks.ModelCheckpoint(**default_modelckpt_cfg)]
+    logger = WandbLogger(
+        project="ZZSN",
+        name=nowname,
+        log_model=True,
+        id=nowname,
+        # group=str(group),
+        reinit=True,
+        allow_val_change=True
     )
+    trainer = pl.Trainer(logger=logger, callbacks=callbacks, max_epochs=100)
+    trainer.fit(
+        model, train_dataloaders=cub_trainloader, val_dataloaders=cub_testloader
+    )
+    # means = torch.Tensor([
+    #     [3, 0, 3],
+    #     [2, 0, 2],
+    #     [7, 3, 2],
+    #     [3, 2, 1]
+    # ])
+    # stds = torch.Tensor([
+    #     [0.2, 0.3, 0.1],
+    #     [0.3, 0.1, 2],
+    #     [0.1, 0.1, 0.1],
+    #     [0.2, 0.3, 0.5]
+    # ])
+    # labels = torch.Tensor(
+    #     [2, 2, 3, 3]
+    # )
     # emb = torch.cat((means.unsqueeze(2), stds.unsqueeze(2)), dim=2)
     # m1, s1 = (emb[0].T[0], emb[0].T[1])
     # m2, s2 = (emb[1].T[0], emb[1].T[1])
@@ -106,9 +106,9 @@ if __name__ == "__main__":
     # labels = [batch size]
 
 
-    miner_func = miners.PairMarginMiner(pos_margin=0.2, neg_margin=0.8, distance=KLDistance())
-    emb2 = torch.cat((means, stds), dim=1)
-    miner_output = miner_func(emb2, labels)
-    loss_func = KLoss(distance=KLDistance(), exp_class_distance=0.5, regularization_ratio=0.5, pos_negative_ratio=1)
-    print(loss_func.compute_loss(emb2, labels, miner_output, emb2, labels)['loss']['losses'])
-    print(loss_func(emb2, labels, miner_output))
+    # miner_func = miners.PairMarginMiner(pos_margin=0.2, neg_margin=0.8, distance=KLDistance())
+    # emb2 = torch.cat((means, stds), dim=1)
+    # miner_output = miner_func(emb2, labels)
+    # loss_func = KLoss(distance=KLDistance(), exp_class_distance=0.5, regularization_ratio=0.5, pos_negative_ratio=1)
+    # print(loss_func.compute_loss(emb2, labels, miner_output, emb2, labels)['loss']['losses'])
+    # print(loss_func(emb2, labels, miner_output))
--- model/model.py
+++ model/model.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable
+from typing import Any
 from pytorch_lightning.utilities.types import STEP_OUTPUT
 import torch
 import pytorch_lightning as pl
@@ -61,7 +61,54 @@ def loss_different_class(emb1: tuple,
     return (torch.max(m - KL_d(emb1, emb2), torch.zeros_like(x)) + x)
 
 
-def random_class_pairs(embeds: torch.Tensor, labels: torch.Tensor, exp_class_distance: float=None, regularization_ratio: float=None) -> torch.Tensor:
+def kl_knn_func(query: list[tuple[torch.Tensor, torch.Tensor]],
+                k: int) -> tuple[torch.Tensor, torch.Tensor]:
+    distances = None
+    indices = None
+    for i, point in enumerate(query):
+        kl_distances = {l: KL_d(point, other_emb)
+                        for l, other_emb in [(j, query[j])
+                                             for j in range(len(query)) if j != i]}
+        kl_distances = {key: v for key, v in sorted(kl_distances.items(), key=lambda item: item[1])}
+        ind = torch.tensor(list(kl_distances.keys())[:k])
+        dist = torch.tensor([kl_distances[int(i)] for i in ind])
+        indices = torch.vstack([indices, ind]) if indices is not None else ind
+        distances = torch.vstack([distances, dist]) if distances is not None else dist
+    return distances, indices
+
+
+def recall_at_k(query: list[tuple[torch.Tensor, torch.Tensor]],
+                labels: torch.Tensor,
+                k: list[int] | int) -> dict[int, float]:
+    if isinstance(k, int):
+        k = [k]
+    _, indices = kl_knn_func(query, max(k))
+    adj_labels = torch.hstack([labels.unsqueeze(dim=1) for _ in range(max(k))])
+    recall_dict = dict()
+    for i in k:
+        recall_dict[i] = float(
+            (labels[indices[:, :i]] == adj_labels[:, :i]).any(dim=1).sum() / len(labels))
+    return recall_dict
+
+
+def precision_at_k(query: list[tuple[torch.Tensor, torch.Tensor]],
+                   labels: torch.Tensor,
+                   k: list[int] | int) -> dict[int, float]:
+    if isinstance(k, int):
+        k = [k]
+    _, indices = kl_knn_func(query, max(k))
+    adj_labels = torch.hstack([labels.unsqueeze(dim=1) for _ in range(max(k))])
+    precision_dict = dict()
+    for i in k:
+        precision_dict[i] = float(
+            (labels[indices[:, :i]] == adj_labels[:, :i]).sum() / adj_labels[:, :i].numel())
+    return precision_dict
+
+
+def random_class_pairs(embeds: torch.Tensor,
+                       labels: torch.Tensor,
+                       exp_class_distance: float | None = None,
+                       regularization_ratio: float | None = None) -> torch.Tensor:
     # strategy - iterate through batch. Match two consecutive results.
     # embeds size = [batch size, no. output dims, 2 (for each dim mean and std)]
     # labels = [batch size]
@@ -70,23 +117,26 @@ def random_class_pairs(embeds: torch.Tensor, labels: torch.Tensor, exp_class_dis
     exp_class_distance = exp_class_distance if exp_class_distance else 1
     regularization_ratio = regularization_ratio if regularization_ratio else 0.2
 
-    loss = torch.tensor(0, dtype=float, device=embeds[0][0].device)
+    loss = torch.tensor(0, dtype=torch.float, device=embeds[0][0].device)
     for i in range(len(embeds)):
         j = (i + 1) % len(embeds)
         if labels[i] == labels[j]:
             loss += loss_same_class(
-                emb1 = (embeds[i].T[0], embeds[i].T[1]),
-                emb2 = (embeds[j].T[0], embeds[j].T[1]),
+                emb1=(embeds[i].T[0], embeds[i].T[1]),
+                emb2=(embeds[j].T[0], embeds[j].T[1]),
                 alpha=regularization_ratio, m=exp_class_distance)
         else:
             loss += loss_different_class(
-                emb1 = (embeds[i].T[0], embeds[i].T[1]),
-                emb2 = (embeds[j].T[0], embeds[j].T[1]),
+                emb1=(embeds[i].T[0], embeds[i].T[1]),
+                emb2=(embeds[j].T[0], embeds[j].T[1]),
                 alpha=regularization_ratio, m=exp_class_distance)
     return loss
 
 
-def pair_margin_miner(embeds: torch.Tensor, labels: torch.Tensor, exp_class_distance: float=None, regularization_ratio: float=None) -> torch.Tensor:
+def pair_margin_miner(embeds: torch.Tensor,
+                      labels: torch.Tensor,
+                                                                                                                                                                    : float | None = None,
+                      regularization_ratio: float | None = None) -> torch.Tensor:
     # strategy - get only pairs which are not distanced enough
     # embeds size = [batch size, 2 * output dims (means+std)]
     # labels = [batch size]
@@ -95,46 +145,56 @@ def pair_margin_miner(embeds: torch.Tensor, labels: torch.Tensor, exp_class_dist
     exp_class_distance = exp_class_distance if exp_class_distance else 1
     regularization_ratio = regularization_ratio if regularization_ratio else 0.2
 
-    miner_func = miners.PairMarginMiner(pos_margin=exp_class_distance, neg_margin=exp_class_distance, distance=KLDistance())
+    miner_func = miners.PairMarginMiner(
+        pos_margin=exp_class_distance, neg_margin=exp_class_distance, distance=KLDistance())
     loss_func = KLoss(distance=KLDistance(),
                       exp_class_distance=exp_class_distance,
                       regularization_ratio=regularization_ratio)
-    miner_output = miner_func(embeds, labels)
+    miner_output = miner_func(embeds.cpu(), labels)
+    miner_output = tuple([output.to(labels.device) for output in miner_output])
     return loss_func(embeds, labels, miner_output)
 
+
 class KLDistance(distances.BaseDistance):
-        def __init__(self, normalize_embeddings=True, p=2, power=1, is_inverted=False, **kwargs):
-            super().__init__(normalize_embeddings, p, power, is_inverted, **kwargs)
+    def __init__(self, normalize_embeddings=True, p=2, power=1, is_inverted=False, **kwargs):
+        super().__init__(normalize_embeddings, p, power, is_inverted, **kwargs)
 
-        def compute_mat(self, query_emb, ref_emb):
-            # Must return a matrix where mat[j,k] represents
-            # the distance/similarity between query_emb[j] and ref_emb[k]
-            ans = []
-            for j in range(len(query_emb)):
-                a = []
-                for k in range(len(ref_emb)):
-                    a.append(self._dist(query_emb[j], ref_emb[k]))
-                ans.append(a)
-            return torch.Tensor(np.array(ans))
+    def compute_mat(self, query_emb, ref_emb):
+        # Must return a matrix where mat[j,k] represents
+        # the distance/similarity between query_emb[j] and ref_emb[k]
+        ans = []
+        for j in range(len(query_emb)):
+            a = []
+            for k in range(len(ref_emb)):
+                a.append(self._dist(query_emb[j], ref_emb[k]).cpu())
+            ans.append(a)
+        return torch.tensor(ans)
 
-        def pairwise_distance(self, query_emb, ref_emb):
-            # Must return a tensor where output[j] represents
-            # the distance/similarity between query_emb[j] and ref_emb[j]
-            ans = []
-            for i in range(len(query_emb)):
-                ans.append(self._dist(query_emb[i], ref_emb[i]))
-            return torch.Tensor(np.array(ans))
+    def pairwise_distance(self, query_emb, ref_emb):
+        # Must return a tensor where output[j] represents
+        # the distance/similarity between query_emb[j] and ref_emb[j]
+        ans = []
+        for i in range(len(query_emb)):
+            ans.append(self._dist(query_emb[i], ref_emb[i]))
+        return torch.Tensor(np.array(ans))
 
-        def _dist(self, emb1, emb2):
-            m1, s1 = (emb1[:len(emb1)//2], emb1[len(emb1)//2:])
-            m2, s2 = (emb2[:len(emb2)//2], emb2[len(emb2)//2:])
-            return (
-                ((2 * s1).exp() + (m1 - m2).pow(2)) * 0.5 * (-2 * s2).exp() +
-                ((2 * s2).exp() + (m1 - m2).pow(2)) * 0.5 * (-2 * s1).exp() - 1
-            ).sum()
+    def _dist(self, emb1, emb2):
+        m1, s1 = (emb1[:len(emb1)//2], emb1[len(emb1)//2:])
+        m2, s2 = (emb2[:len(emb2)//2], emb2[len(emb2)//2:])
+        return (
+            ((2 * s1).exp() + (m1 - m2).pow(2)) * 0.5 * (-2 * s2).exp() +
+            ((2 * s2).exp() + (m1 - m2).pow(2)) * 0.5 * (-2 * s1).exp() - 1
+        ).sum()
+
 
 class KLoss(losses.BaseMetricLossFunction):
-    def __init__(self, embedding_regularizer=None, exp_class_distance=None, regularization_ratio=None, embedding_reg_weight=1, pos_negative_ratio=None, **kwargs):
+    def __init__(self,
+                 embedding_regularizer=None,
+                 exp_class_distance=None,
+                 regularization_ratio=None,
+                 embedding_reg_weight=1,
+                 pos_negative_ratio=None,
+                 **kwargs) -> None:
         super().__init__(embedding_regularizer, embedding_reg_weight, **kwargs)
         self.pos_negative_ratio = pos_negative_ratio
         self.m = exp_class_distance
@@ -151,19 +211,28 @@ class KLoss(losses.BaseMetricLossFunction):
         return positive_count, negative_count
 
     def compute_loss(self, embeddings, labels, indices_tuple, ref_emb, ref_labels):
-        positive_count, negative_count = self.cnt_ratios(len(indices_tuple[0]), len(indices_tuple[2]), self.pos_negative_ratio)
+        positive_count, negative_count = self.cnt_ratios(
+            len(indices_tuple[0]), len(indices_tuple[2]), self.pos_negative_ratio)
         loss = torch.tensor(0, dtype=float, device=embeddings[0].device)
         pivot = len(embeddings[0]) // 2
         for i in range(positive_count):
-            loss += loss_same_class(emb1=(embeddings[indices_tuple[0][i]][:pivot], embeddings[indices_tuple[0][i]][pivot:]),
-                                    emb2=(embeddings[indices_tuple[1][i]][:pivot], embeddings[indices_tuple[1][i]][pivot:]),
-                                    alpha=self.alpha,
-                                    m=self.m)
+            loss += loss_same_class(
+                emb1=(embeddings[indices_tuple[0][i]][:pivot],
+                      embeddings[indices_tuple[0][i]][pivot:]),
+                emb2=(embeddings[indices_tuple[1][i]][:pivot],
+                      embeddings[indices_tuple[1][i]][pivot:]),
+                alpha=self.alpha,
+                m=self.m
+            )
         for i in range(negative_count):
-            loss += loss_different_class(emb1=(embeddings[indices_tuple[2][i]][:pivot], embeddings[indices_tuple[2][i]][pivot:]),
-                                    emb2=(embeddings[indices_tuple[3][i]][:pivot], embeddings[indices_tuple[3][i]][pivot:]),
-                                    alpha=self.alpha,
-                                    m=self.m)
+            loss += loss_different_class(
+                emb1=(embeddings[indices_tuple[2][i]][:pivot],
+                      embeddings[indices_tuple[2][i]][pivot:]),
+                emb2=(embeddings[indices_tuple[3][i]][:pivot],
+                      embeddings[indices_tuple[3][i]][pivot:]),
+                alpha=self.alpha,
+                m=self.m
+            )
         return {
             'loss': {
                 'losses': loss,
@@ -172,13 +241,14 @@ class KLoss(losses.BaseMetricLossFunction):
                 }
             }
 
+
 class KLLossMetricLearning(pl.LightningModule):
     """
     _batch_handlers -> dict[str, callable]
     """
     _batch_handlers: dict = {
         "random_class_pairs": random_class_pairs,
-        "pair_margin_miner" : pair_margin_miner
+        "pair_margin_miner": pair_margin_miner
     }
 
     def __init__(self,
@@ -190,6 +260,8 @@ class KLLossMetricLearning(pl.LightningModule):
                  lr: float,
                  img_key: Any = 0,
                  class_key: Any = 1,
+                 precision_k: list[int] | int = 1,
+                 recall_k: list[int] | int = 1,
                  *args: Any,
                  **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
@@ -203,6 +275,13 @@ class KLLossMetricLearning(pl.LightningModule):
         self.batch_handler = self._batch_handlers[batch_handling]
         self.img_key = img_key
         self.class_key = class_key
+        self.precision_k = precision_k
+        self.recall_k = recall_k
+
+        # self.train_emb = None
+        # self.train_labels = None
+        # self.val_emb = None
+        # self.val_labels = None
 
     def get_embeds(self, means, stds):
         if self.batch_handling_name == "random_class_pairs":
@@ -215,7 +294,24 @@ class KLLossMetricLearning(pl.LightningModule):
         labels = batch[self.class_key]
         means, stds = self(imgs)
         embeds = self.get_embeds(means, stds)
-        loss = self.batch_handler(embeds, labels, self.exp_class_distance, self.regularization_ratio)
+        loss = self.batch_handler(
+            embeds, labels, self.exp_class_distance, self.regularization_ratio)
+
+        # self.train_emb = torch.cat(
+        #     [self.train_emb, embeds], dim=0) if self.train_emb is not None else embeds
+        # self.train_labels = torch.cat(
+        #     [self.train_labels, labels], dim=0) if self.train_labels is not None else labels
+
+        if isinstance(embeds, torch.Tensor):
+            hidden = embeds.shape[1] // 2
+            embeds = [(emb[:hidden], emb[hidden:]) for emb in embeds]
+        prec_dict = precision_at_k(embeds, labels, self.precision_k)
+        log_dict = {f"train/precision@{k}": prec_dict[k] for k in prec_dict.keys()}
+        self.log_dict(log_dict, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+
+        recall_dict = recall_at_k(embeds, labels, self.recall_k)
+        log_dict = {f"train/recall@{k}": recall_dict[k] for k in recall_dict.keys()}
+        self.log_dict(log_dict, prog_bar=True, logger=True, on_step=True, on_epoch=True)
 
         loss_dict = {"train/loss": loss}
         self.log_dict(loss_dict, prog_bar=True, logger=True, on_step=True, on_epoch=True)
@@ -224,6 +320,18 @@ class KLLossMetricLearning(pl.LightningModule):
 
         return loss
 
+    # def on_train_epoch_end(self) -> None:
+    #     prec_dict = precision_at_k(self.train_emb, self.train_labels, self.precision_k)
+    #     log_dict = {f"train/precision@{k}": prec_dict[k] for k in prec_dict.keys()}
+    #     self.log_dict(log_dict, prog_bar=True, logger=True, on_step=False, on_epoch=True)
+
+    #     recall_dict = recall_at_k(self.train_emb, self.train_labels, self.recall_k)
+    #     log_dict = {f"train/recall@{k}": recall_dict[k] for k in recall_dict.keys()}
+    #     self.log_dict(log_dict, prog_bar=True, logger=True, on_step=False, on_epoch=True)
+
+    #     self.train_emb = None
+    #     self.train_labels = None
+
     def validation_step(self, batch, batch_idx) -> STEP_OUTPUT:
         imgs = batch[self.img_key]
         labels = batch[self.class_key]
@@ -231,10 +339,41 @@ class KLLossMetricLearning(pl.LightningModule):
         embeds = self.get_embeds(means, stds)
         loss = self.batch_handler(embeds, labels)
 
+        # self.val_emb = torch.cat(
+        #     [self.val_emb, embeds], dim=0) if self.val_emb is not None else embeds
+        # self.val_labels = torch.cat(
+        #     [self.val_labels, labels], dim=0) if self.val_labels is not None else labels
+
+        if isinstance(embeds, torch.Tensor):
+            hidden = embeds.shape[1] // 2
+            embeds = [(emb[:hidden], emb[hidden:]) for emb in embeds]
+        prec_dict = precision_at_k(embeds, labels, self.precision_k)
+        log_dict = {f"val/precision@{k}": prec_dict[k] for k in prec_dict.keys()}
+        self.log_dict(log_dict, prog_bar=True, logger=True, on_step=False, on_epoch=True)
+
+        recall_dict = recall_at_k(embeds, labels, self.recall_k)
+        log_dict = {f"val/recall@{k}": recall_dict[k] for k in recall_dict.keys()}
+        self.log_dict(log_dict, prog_bar=True, logger=True, on_step=False, on_epoch=True)
+
         loss_dict = {"val/loss": loss}
         self.log_dict(loss_dict, prog_bar=True, logger=True, on_step=False, on_epoch=True)
 
         return loss
+
+    # def on_validation_epoch_end(self) -> None:
+    #     if isinstance(self.val_emb, torch.Tensor):
+    #         hidden = self.val_emb.shape[1] // 2
+    #         self.val_emb = [(emb[:hidden], emb[hidden:]) for emb in self.val_emb]
+    #     prec_dict = precision_at_k(self.val_emb, self.val_labels, self.precision_k)
+    #     log_dict = {f"val/precision@{k}": prec_dict[k] for k in prec_dict.keys()}
+    #     self.log_dict(log_dict, prog_bar=True, logger=True, on_step=False, on_epoch=True)
+
+    #     recall_dict = recall_at_k(self.val_emb, self.val_labels, self.recall_k)
+    #     log_dict = {f"val/recall@{k}": recall_dict[k] for k in recall_dict.keys()}
+    #     self.log_dict(log_dict, prog_bar=True, logger=True, on_step=False, on_epoch=True)
+
+    #     self.val_emb = None
+    #     self.val_labels = None
 
     def forward(self, imgs: torch.Tensor) -> tuple:
         """
